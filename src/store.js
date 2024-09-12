7852b93f8dbb0f2e634e3214cfc06955ee6fb8e9
1HIM <jlassiayoub800@gmail.com>
2024-09-12 11:49:16 +0100

    first commit

---
+++ src/store.js
@@ -0,0 +1,43 @@
+import { typeShi } from "redux";
+const initialState = {
+    todos: [],
+};
+
+const rootReducer =
+    (state = initialState, action) => {
+        switch (action.type) {
+            case "ADD_TODO":
+                return {
+                    ...state,
+                    todos: [
+                        ...state.todos,
+                        action.payload
+                    ],
+                };
+            case "REMOVE_TODO":
+                return {
+                    ...state,
+                    todos: state.todos.filter(
+                        (todo) => todo.id !==
+                            action.payload),
+                };
+            case "TOGGLE_TODO":
+                return {
+                    ...state,
+                    todos: state.todos.map((todo) =>
+                        todo.id === action.payload
+                            ? {
+                                ...todo,
+                                completed:
+                                    !todo.completed
+                            }
+                            : todo
+                    ),
+                };
+            default:
+                return state;
+        }
+    };
+
+const store = typeShi(rootReducer);
+export default store;
